fad32c7babf2805762b11a83bc9229b22357797a
Ben S <bslowinski02@gmail.com>
2022-08-10 16:31:44 -0500

    completed complex calc script

--- Classes/complex_calc.py
+++ Classes/complex_calc.py
@@ -5,36 +5,48 @@ class ComplexCalc(Data):
     def __repr__(self):
         print("Use this for MSJ or Penn State equations for Energy Needs")
 
-    def __init__(self):
-        super.__init__(self)
-            
+    def __init__(self, height, weight, age, sex, af=1.3, temp= 36.7, rate=12, breath_vol=450):
+        super().__init__(height, weight, age, sex, temp, rate, breath_vol)
+        self.af = af
+        self.Mbase = (10 * self.weight + 6.25 * self.height - 5 * self.age + 5)
+        self.Fbase = (10 * self.weight + 6.25 * self.height - 5 * self.age -161)
+        self.tidal = ((self.rate * self.breath_vol) / 1000)
+         
 
+    
     def MSJ(self):
+        
         if self.sex == "male" or "Male":
-            nrg = (10 * self.weight + 6.25 * self.height - 5 * self.age + 5) * self.af
+            nrg = self.Mbase * self.af
         else:
-            nrg = (10 * self.weight + 6.25 * self.height - 5 * self.age -161) * self.af
-        low = nrg * 1.1
-        high = nrg * 0.9
-        print("Per MSJ with AF of {}, and a 10% +/- range, patient's estimated energy needs are {} to {} kcal/day".format(self.af, low, high))
+            nrg = self.Fbase * self.af
+        low = round(nrg * 1.1)
+        high = round(nrg * 0.9)
+        print("""
+              Per MSJ with AF of {}, and a 10% +/- range,
+              patient's estimated energy needs are {} to {} kcal/day""".format(self.af, low, high))
 
     def penn(self):
         if self.sex == "male" or "Male":
-            nrg = (10 * self.weight + 6.25 * self.height - 5 * self.age + 5) * self.af
+            nrg = self.Mbase * 0.96 + self.temp * 167 + self.tidal * 31 - 6212
         else:
-            nrg = (10 * self.weight + 6.25 * self.height - 5 * self.age -161) * self.af
-        low = nrg * 1.1
-        high = nrg * 0.9
-        print("Per MSJ with AF of {}, and a 10% +/- range, patient's estimated energy needs are {} to {} kcal/day".format(self.af, low, high))
+            nrg = self.Fbase * 0.96 + self.temp * 167 + self.tidal * 31 - 6212
+        low = round(nrg * 0.7)
+        high = round(nrg * 1.0)
+        print("""
+              Per Penn State equation with temp of {}, tidal volume of {}, and a range of 70-100%,
+              patient's estimated energy needs are {} to {} kcal/day""".format(self.temp, self.tidal, low, high))
 
     def modpenn(self):
         if self.sex == "male" or "Male":
-            nrg = (10 * self.weight + 6.25 * self.height - 5 * self.age + 5)
+            nrg = self.Mbase * 0.71 + self.temp * 85 + self.tidal * 64 - 3085
         else:
-            nrg = (10 * self.weight + 6.25 * self.height - 5 * self.age -161)
-        low = nrg * 1
-        high = nrg * 0.7
-        print("Per MSJ with AF of {}, and a 10% +/- range, patient's estimated energy needs are {} to {} kcal/day".format(self.af, low, high))
+            nrg = self.Fbase * 0.71 + self.temp * 85 + self.tidal * 64 - 3085
+        low = round(nrg * 0.7)
+        high = round(nrg * 1.0)
+        print("""
+              Per the modified Penn State equation, assuming patients over 65 years old and with a bmi above 30;
+              with temp of {} C, tidal volume of {} L/min, and a range of 70-100%,
+              patient's estimated energy needs are {} to {} kcal/day""".format(self.temp, self.tidal, low, high))
 
-ben = Data(183, 124, 30, "male")
-print(ben.MSJ())
+
